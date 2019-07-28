f441e6d9c13557ca336f527148750b806da88959
josepablosm <josepablosm@gmail.com>
2019-07-28 16:08:39 -0600

    Add Photo Gallery

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -1,16 +1,27 @@
 import {Routes} from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './Members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { MemberDetailComponent } from './Members/member-detail/member-detail.component';
+import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
+import { MemberListResolver } from './_resolvers/member-list.resolver';
 
 export const appRoutes: Routes = [
 {path: 'home', component: HomeComponent},
-{path: 'members', component: MemberListComponent , canActivate: [AuthGuard] },
-{path: 'messages', component: MessagesComponent},
-{path: 'lists', component: ListsComponent},
-{path: '**', redirectTo: 'home', pathMatch: 'full'},
+{ path: '',
+runGuardsAndResolvers: 'always',
+canActivate: [AuthGuard],
+children: [
+  {path: 'members', component: MemberListComponent, resolve: {users: MemberListResolver}  },
+  {path: 'members/:id', component: MemberDetailComponent,
+         resolve: {user: MemberDetailResolver}},
+  {path: 'messages', component: MessagesComponent},
+  {path: 'lists', component: ListsComponent},
+
+]},
+  { path: '**', redirectTo: 'home', pathMatch: 'full'},
 
 ];
 
